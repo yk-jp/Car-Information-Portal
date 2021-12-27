d0d88e8b7196c2eafdb2171aef9134b5b269ce08
yusuke <ID+username@users.noreply.github.com>
2021-12-27 10:57:26 -0800

    set up year from 1886 to current year

--- base/views.py
+++ base/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context
 import requests
 import json
+# importing date class from datetime module
+from datetime import date
 # custom modules
 from .config.config import config
 from .methods.methods import RestCarQueryAPI
+
+todays_date = date.today() #today's date
 
 
 # Create your views here.
@@ -14,15 +19,22 @@ def index(request):
     # req = RestCarQueryAPI.getModels()
 
     # print(req.status_code)
-
-    return render(request,'pages/index.html',{'carMakers':carNakers})
+    context = {
+        'carMakers':carNakers,
+        'years':range(1886,todays_date.year + 1)
+    }
+    return render(request,'pages/index.html',context)
 
 def modelList(request,make):
     carNakers = config.get('GermanCarMaker') 
     if request.method == 'GET':
         req = RestCarQueryAPI.getModels(make)
         models = json.loads(req.text[2:-2]).get('Models')
-        print(models)
-        return render(request,'pages/modelList.html',{'carMakers':carNakers ,'make':make,'models':models})
+        context = {
+            'carMakers':carNakers,
+            'make':make,
+            'models':models
+        }
+        return render(request,'pages/modelList.html',context)
 
     return render(request,'pages/modelList.html',{'carMakers':carNakers})
