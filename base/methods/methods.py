539c63d5f0b0ed356ac8ba3ebc85bcfeff0db740
yusuke <ID+username@users.noreply.github.com>
2021-12-28 17:16:43 -0800

    project is completely done.

--- base/methods/methods.py
+++ base/methods/methods.py
@@ -6,12 +6,17 @@ class RestCarQueryAPI():
     baseUrl = config.get('externalCarAPI').get('baseUrl')
     Headers = {'User-Agent': 'Mozilla/5.0'}
 
-    def getModels(make=None,min_year=date.today().year,max_year=date.today().year) -> str:
-        url = RestCarQueryAPI.baseUrl + '&cmd=getTrims&model'
+    def getModels(make=None,model=None,min_year=date.today().year,max_year=date.today().year) -> str:
+        url = RestCarQueryAPI.baseUrl + '&cmd=getTrims'
         if(make):
             url += '&make=' + make
-        url += '&{min_year}&{max_year}'
+        if(model):
+            url += '&model=' + model
+        if(min_year == None):
+            min_year = date.today().year
+        url += '&min_year=' + str(min_year)
+        if(max_year == None):
+            max_year = date.today().year
+        url += '&max_year=' + str(max_year)
         return RestCarQueryAPI.session.get(url, headers = RestCarQueryAPI.Headers)
-
-    
 
--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('modelList/<str:make>',views.modelList,name='modelList')
+    path('modelList/<str:make>',views.modelList,name='modelList'),
+    path('modelList/<str:make>/<str:model>',views.modelDetails,name='modelDetails')
 ]
